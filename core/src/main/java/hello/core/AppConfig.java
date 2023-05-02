81d297bb392ae701f58a5f1789d4144d53ea4eb9
wnd06 <min990605@naver.com>
2023-05-02 21:46:55 +0900

    스프링 전환!

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,26 +9,36 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * 인터페이스에만 의존하도록 코드를 설정 해놓은 상태지만 구현체가 없다
  * 그래서 해결방안으로 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해줘야한다.
  * -> 이 코드가 구현 객체를 생성하고, 연결하는 책임을 가지는 클래스이다.
+ * - 스프링으로 전환 하기 -
+ * AppConfig에 설정을 구성한다는 뜻의 @Configuration 을 붙여준다.
+ * 각 메서드에 @Bean 을 붙여준다. 이렇게 하면 스프링 컨테이너에 스프링 빈으로 등록된다.
+ * 등록된 스프링 빈을 사용하기 위해서는 먼저 스프링 컨테이너를 불러온 후 getBean을 활용하여 사용할 수 있다.
  */
+@Configuration
 public class AppConfig {
+    @Bean
      public MemberService memberService() {
          return new MemberServiceImpl(memberRepository());
      }
-
+    @Bean
      public OrderService orderService() {
          return new OrderServiceImpl(
                  memberRepository(), discountPolicy());
      }
-
+    @Bean
      public MemberRepository memberRepository() {
          return new MemoryMemberRepository();
      }
-
+    @Bean
      public DiscountPolicy discountPolicy() {
 //         return new FixDiscountPolicy();
          return new RateDiscountPolicy(); //할인 정책 변경 하면 코드를 일일이 변경할 필요가 없다.
--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,12 +7,19 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        //applicationContext -> 스프링 컨테이너
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //@Bean이 붙어있는 메서드를 찾기위해 applicationContext.getBean 사용하였다.
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
